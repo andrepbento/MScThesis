8b4125e59d860f0f20416292ddf0344d0e8a534e
André Pascoal Bento <andrebento1995@sapo.pt>
2019-03-06 14:09:32 +0000

    Changed .gitignore file.

---
+++ Graphy/tests/db/test_arangodb.py
@@ -0,0 +1,14 @@
+"""
+    Author: André Bento
+    Date last modified: 04-03-2019
+"""
+from unittest import TestCase
+
+
+class TestArango(TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+    def tearDown(self) -> None:
+        super().tearDown()
---
+++ Graphy/tests/db/test_opentsdb.py
@@ -0,0 +1,14 @@
+"""
+    Author: André Bento
+    Date last modified: 01-03-2019
+"""
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
